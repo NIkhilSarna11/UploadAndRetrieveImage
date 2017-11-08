26f9bcb9ead4e383eb43c6e02574afe022030080
NikhilSarna11 <nikhil.sarna11@gmail.com>
2017-11-08 18:51:41 +0530

    Upload and retrieve images

---
+++ java/com/example/nikhil/uploadandretrieveimage/Constants.java
@@ -0,0 +1,14 @@
+package com.example.nikhil.uploadandretrieveimage;
+
+/**
+ * Created by nikhil on 7/11/17.
+ */
+
+public class Constants {
+
+
+
+
+    public static final String STORAGE_PATH_UPLOADS = "uploads/";
+    public static final String DATABASE_PATH_UPLOADS = "uploads";
+}
